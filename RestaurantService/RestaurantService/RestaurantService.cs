b9b0f40ab8dd6665a7b3798c954111f837c70cbc
Regina Reustle <rreustle@recom.eu>
2022-03-04 12:41:20 +0100

    First requirement: Add Table

---
+++ RestaurantService/RestaurantService/RestaurantService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantService
+{
+    public class RestaurantService
+    {
+        private readonly IList<Table> _tables;
+
+        public RestaurantService()
+        {
+            _tables = new List<Table>();
+        }
+
+        public void AddTable(int tableNumber, int numberOfSeats)
+        {
+            var table = new Table();
+            table.TableNumber = tableNumber;
+            table.NumberOfSeats = numberOfSeats;
+
+            _tables.Add(table);
+
+        }
+
+        public IList<Table> GetAllTables()
+        {
+            return _tables;
+        }
+    }
+
+    public class Table
+    {
+        public int TableNumber { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
---
+++ RestaurantService/RestaurantServiceTests/RestaurantServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RestaurantServiceTests
+{
+    [TestFixture]
+    class RestaurantServiceTests
+    {
+        [Test]
+        public void AddTable_Verify()
+        {
+            // Arrange
+            var testClass = new RestaurantService.RestaurantService();
+            int tableNumber = 1;
+            int numberOfSeats = 4;
+
+            // Precondition
+            testClass.GetAllTables().Should().BeEmpty();
+
+            // Act
+            testClass.AddTable(tableNumber, numberOfSeats);
+
+            // Assert
+            var tables = testClass.GetAllTables();
+            tables.Should().HaveCount(1);
+            var table = tables.First();
+            table.TableNumber.Should().Be(tableNumber);
+            table.NumberOfSeats.Should().Be(numberOfSeats);
+        }
+    }
+}
